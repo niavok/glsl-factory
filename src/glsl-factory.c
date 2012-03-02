01fca79954cde8dd36b9f93f554cc2185023aff4
Frédéric Bertolus <fred.bertolus@gmail.com>
2012-03-02 12:16:39 +0100

    remove gtk gui

--- src/glsl-factory.c
+++ src/glsl-factory.c
@@ -1,32 +1,14 @@
 /* GLSL Factory Main */
 #include <stdio.h>
-#include "gui.h"
 #include "renderer.h"
 
 int main(int argc, char *argv[]) {
+   
+    RendererDescriptor * descriptor;
 
-
-    int pid;
-
-    if ((pid = fork()) == 0) {
-        /* Child */
-        /*Editor window*/
-
-        GuiDescriptor * descriptor;
-
-        descriptor = gui_init(argc, argv);
-        gui_run(descriptor);
-        gui_destroy(descriptor);
-        
-    } else {
-        /* Father */
-        /*SDL/OpenGL window*/
-        RendererDescriptor * descriptor;
-
-        descriptor = renderer_init(argc, argv);
-        renderer_run(descriptor);
-        renderer_destroy(descriptor);
-    }
+    descriptor = renderer_init(argc, argv);
+    renderer_run(descriptor);
+    renderer_destroy(descriptor);
     
     return 0;
 }
